eaffb5e5e999d8bb664d5e4fb2aa7895380c54ef
Виталий Мигаев <kitalyans1@gmail.com>
2025-04-14 16:35:46 +0300

    14.04.25

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import Category, Post, Location, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+        'text',
+        'author',
+        'pub_date',
+        'location',
+        'category',
+        'is_published',
+        'created_at'
+    )
+    list_editable = (
+        'text',
+        'category'
+    )
+    search_fields = ('title', 'text')
+    list_filter = ('category', 'author')
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
